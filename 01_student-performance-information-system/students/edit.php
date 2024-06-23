f5ebeeeb7ff6d3a062acd4baf05ae601f96f47f1
Shihab Mahamud <67628903+p-nerd@users.noreply.github.com>
2024-06-23 14:49:03 +0600

    update students operation

--- 01_student-performance-information-system/students/edit.php
+++ 01_student-performance-information-system/students/edit.php
@@ -1,18 +1,23 @@
 <?php
+
 require_once "../views/layouts/header.php";
+
+$id = $_REQUEST["id"];
+
+$student = $db->query("SELECT * FROM students WHERE id=:id", ["id" => $id])->find();
 ?>
 <div class="rounded-lg border bg-card text-card-foreground shadow-sm w-full max-w-md" data-v0-t="card">
     <div class="flex flex-col space-y-1.5 p-6">
-        <h3 class="whitespace-nowrap text-2xl font-semibold leading-none tracking-tight">Student Registration</h3>
+        <h3 class="whitespace-nowrap text-2xl font-semibold leading-none tracking-tight">Update Student Info</h3>
         <p class="text-sm text-muted-foreground">Fill out the form below to register as a new student.</p>
     </div>
-    <form class="p-6 space-y-4" method="post" action="store.php">
+    <form class="p-6 space-y-4" method="post" action="/students/update.php?id=<?= $id ?>">
         <div class="grid grid-cols-2 gap-4">
             <div class="space-y-2">
                 <label class="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70" for="firstName">
                     First Name
                 </label>
-                <input value="<?= $old("first_name") ?>" name="first_name" class="flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50" id="firstName" placeholder="Enter your first name" />
+                <input value="<?= $old("first_name") ?? $student['first_name'] ?>" name="first_name" class="flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50" id="firstName" placeholder="Enter your first name" />
 
                 <span class="text-xs text-red-500"><?= $error('first_name') ?></span>
             </div>
@@ -20,7 +25,7 @@ require_once "../views/layouts/header.php";
                 <label class="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70" for="lastName">
                     Last Name
                 </label>
-                <input value="<?= $old("last_name") ?>" name="last_name" class="flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50" id="lastName" placeholder="Enter your last name" />
+                <input value="<?= $old("last_name") ?? $student['last_name'] ?>" name="last_name" class="flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50" id="lastName" placeholder="Enter your last name" />
 
                 <span class="text-xs text-red-500"><?= $error('last_name') ?></span>
             </div>
@@ -29,18 +34,18 @@ require_once "../views/layouts/header.php";
             <label class="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70" for="email">
                 Email
             </label>
-            <input value="<?= $old("email") ?>" name="email" class="flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50" id="email" placeholder="Enter your email" type="email" />
+            <input value="<?= $old("email") ?? $student['email'] ?>" name="email" class="flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50" id="email" placeholder="Enter your email" type="email" />
             <span class="text-xs text-red-500"><?= $error('email') ?></span>
         </div>
         <div class="space-y-2">
             <label class="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70" for="phone">
                 Phone Number
             </label>
-            <input value="<?= $old("phone_number") ?>" name="phone_number" class="flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50" id="phone" placeholder="Enter your phone number" type="tel" />
+            <input value="<?= $old("phone_number") ?? $student['phone_number'] ?>" name="phone_number" class="flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50" id="phone" placeholder="Enter your phone number" type="tel" />
 
             <span class="text-xs text-red-500"><?= $error('phone_number') ?></span>
         </div>
-        <button type="submit" class="w-full py-2 px-4 bg-black text-white rounded">Create Student</button>
+        <button type="submit" class="w-full py-2 px-4 bg-black text-white rounded">Update Student</button>
     </form>
 </div>
 
---
+++ 01_student-performance-information-system/students/update.php
@@ -0,0 +1,50 @@
+<?php
+
+require_once "../boot.php";
+
+use Core\Error;
+use Core\Old;
+use Core\Validate;
+
+$id = $_REQUEST["id"];
+
+$errors = [];
+
+$first_name = Validate::string($_POST["first_name"]);
+$last_name = Validate::string($_POST["last_name"]);
+$email = Validate::email($_POST["email"]);
+$phone_number = Validate::phoneNumber($_POST["phone_number"]);
+
+if (!$first_name) {
+    $errors["first_name"] = "The first name is required";
+}
+if (!$last_name) {
+    $errors["last_name"] = "The last name is required";
+}
+if (!$email) {
+    $errors["email"] = "The email have to be valid";
+}
+if (!$phone_number) {
+    $errors["phone_number"] = "The email have to be valid";
+}
+
+$student = $db->query("SELECT * FROM students WHERE email=:email", ["email" => $email])->find();
+if ($student["id"] !== (int)$id) {
+    $errors["email"] = "The email already exists";
+}
+
+if (!empty($errors)) {
+    Old::set($_POST);
+    Error::set("validation error", $errors);
+    redirect("/students/edit.php?id=$id");
+}
+
+$db->query('UPDATE students SET first_name=:first_name, last_name=:last_name, email=:email, phone_number=:phone_number WHERE id=:id', [
+    "id" => $id,
+    "first_name" => $first_name,
+    "last_name" => $last_name,
+    "email" => $email,
+    "phone_number" => $phone_number,
+]);
+
+redirect("/students/index.php");
