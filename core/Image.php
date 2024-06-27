fa7fe37fdc445165a793acbf346829d90f78df65
Shihab Mahamud <67628903+p-nerd@users.noreply.github.com>
2024-06-27 10:14:52 +0600

    image upload bug fixed

--- core/Image.php
+++ core/Image.php
@@ -21,18 +21,15 @@ class Image
     public static function save($file)
     {
         $targetFile = self::BASE_DIR . basename($file["name"]);
-        $uploadOk = 1;
 
         // Check if file already exists
         if (file_exists($targetFile)) {
             $targetFile = self::BASE_DIR . time() . basename($file["name"]);
-            $uploadOk = 0;
         }
 
         // Check file size (e.g., limit to 500KB)
         if ($file["size"] > 500000) {
             throw new Exception("Sorry, your file is too large.");
-            $uploadOk = 0;
         }
 
         $imageFileType = strtolower(pathinfo($targetFile, PATHINFO_EXTENSION));
@@ -40,20 +37,13 @@ class Image
         $allowedTypes = array("jpg", "png", "jpeg", "gif");
         if (!in_array($imageFileType, $allowedTypes)) {
             throw new Exception("Sorry, only JPG, JPEG, PNG & GIF files are allowed.");
-            $uploadOk = 0;
         }
 
-        // Check if $uploadOk is set to 0 by an error
-        if ($uploadOk == 0) {
-            throw new Exception("Sorry, your file was not uploaded.");
-            // If everything is ok, try to upload file
-        } else {
-            if (move_uploaded_file($file["tmp_name"], $targetFile)) {
-                return htmlspecialchars(basename($file["name"]));
-            } else {
-                throw new Exception("Sorry, there was an error uploading your file.");
-            }
+        if (!move_uploaded_file($file["tmp_name"], $targetFile)) {
+            throw new Exception("Sorry, there was an error uploading your file.");
         }
+
+        return htmlspecialchars(basename($file["name"]));
     }
 
     public static function path(string $imageName): string
