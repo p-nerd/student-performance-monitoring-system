c1326b1e64ede8e116c78ba4e2658a1e4722b88a
Shihab Mahamud <67628903+p-nerd@users.noreply.github.com>
2024-07-06 01:44:05 +0600

    improve conf class

--- core/Conf.php
+++ core/Conf.php
@@ -6,18 +6,25 @@ class Conf
 {
     protected const BASE_DIR = __DIR__ . "/../conf/";
 
-    public static function get(string $name): string
+    public static function get(string $name): ?string
     {
         $parts = explode(".", $name);
 
-        $confFileName = $parts[0];
+        if (count($parts) < 2) {
+            return null;
+        }
 
+        $confFileName = $parts[0];
         $data = require static::BASE_DIR . "/$confFileName.php";
 
+        // Traverse the array based on the parts
         for ($i = 1; $i < count($parts); $i++) {
+            if (!isset($data[$parts[$i]])) {
+                return null;
+            }
             $data = $data[$parts[$i]];
         }
 
-        return $data ?? null;
+        return $data;
     }
 }
